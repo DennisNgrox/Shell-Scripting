9f75266562ec00edb37b912d1c754211c072b1d6
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2023-01-19 12:01:27 -0300

    Update triggerprototype_update.sh

--- triggerprototype_update.sh
+++ triggerprototype_update.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 
-#5 ° Preciso da Triggerid para update de tags
-#4 ° Preciso do DiscoveryID para get da trigger // FEITO
+#1 ° Preciso da Triggerid para update de tags
+#2 ° Preciso do DiscoveryID para get da trigger // FEITO
 #3 ° Preciso do Hostids para get do discoveryID // FEITO
-#2 ° Lista com nome dos templates. // FEITO COM $1 --- Realiza utilizando: sed 
-#1 ° Fazer template.get // FEITO
+#4 ° Lista com nome dos templates. // FEITO COM $1 --- Realiza utilizando: sed 
+#5 ° Fazer template.get // FEITO
 
 
 zabbix_user="Admin"
-zabbix_pass="Sales@2022"
+zabbix_pass="ip"
 zabbix_api="http://ip/api_jsonrpc.php"
+PRIMEIRO=$1
+SEGUNDO=$2
 
 
-zabbix_auth_token=$(curl -s -H  'Content-Type: application/json-rpc' -d "{\"jsonrpc\": \"2.0\",\"method\":\"user.login\",\"params\":{\"user\":\""${zabbix_user}"\",\"password\":\""${zabbix_pass}"\"},\"auth\": null,\"id\":0}" $zabbix_api |  jq -r .result)
+zabbix_auth_token=$(curl -s -k -H  'Content-Type: application/json-rpc' -d "{\"jsonrpc\": \"2.0\",\"method\":\"user.login\",\"params\":{\"user\":\""${zabbix_user}"\",\"password\":\""${zabbix_pass}"\"},\"auth\": null,\"id\":0}" $zabbix_api |  jq -r .result)
 
-hostid=$(curl -s -H  'Content-Type: application/json-rpc' -d "
+hostid=$(curl -s -k -H  'Content-Type: application/json-rpc' -d "
 {
     \"jsonrpc\": \"2.0\",
     \"method\": \"template.get\",
@@ -23,7 +25,7 @@ hostid=$(curl -s -H  'Content-Type: application/json-rpc' -d "
         \"output\": \"["hostid"]\",
         \"filter\": {
             \"host\": [
-                \"$1\"
+                \"${PRIMEIRO}\"
             ]
         }
     },
@@ -31,7 +33,7 @@ hostid=$(curl -s -H  'Content-Type: application/json-rpc' -d "
     \"id\": 1
 }" ${zabbix_api}| awk -F\" '{print $10}') 
 
-discoveryid=$(curl -s -H  'Content-Type: application/json-rpc' -d "
+discoveryid=$(curl -s -k -H  'Content-Type: application/json-rpc' -d "
 {
     \"jsonrpc\": \"2.0\",
     \"method\": \"discoveryrule.get\",
@@ -41,37 +43,51 @@ discoveryid=$(curl -s -H  'Content-Type: application/json-rpc' -d "
     },
     \"auth\": \"${zabbix_auth_token}\",
     \"id\": 1
-}" ${zabbix_api}| awk -F\" '{print $10}')
+}" ${zabbix_api}| jq .result[].itemid | sed s'/"//g' > teste.dennis)
 
+for i in $(cat teste.dennis)
+do 
+    value=$(echo $i);
+    triggerid=$(curl -s -k -H  'Content-Type: application/json-rpc' -d "
+        {
+        \"jsonrpc\": \"2.0\",
+        \"method\": \"triggerprototype.get\",
+        \"params\": {
+            \"output\": [\"triggerid\"],
+            \"discoveryids\": \"${value}\"
+        },
+        \"auth\": \"${zabbix_auth_token}\",
+        \"id\": 1
+        }" ${zabbix_api} | jq .result[].triggerid | sed s'/"//g' > valor.trigger);
 
-triggerid=$(curl -s -H  'Content-Type: application/json-rpc' -d "
-{
-    \"jsonrpc\": \"2.0\",
-    \"method\": \"triggerprototype.get\",
-    \"params\": {
-        \"output\": [\"triggerid\"],
-        \"discoveryids\": \"${discoveryid}\"
-    },
-    \"auth\": \"${zabbix_auth_token}\",
-    \"id\": 1
-}" ${zabbix_api} | awk -F\" '{print $10}')
+    for a in $(cat valor.trigger);
+    do
+         trigg=$(echo $a);
+         tagupdate=$(curl -s -k -H  'Content-Type: application/json-rpc' -d "
+        {
+            \"jsonrpc\": \"2.0\",
+            \"method\": \"triggerprototype.update\",
+            \"params\": {
+                \"triggerid\": \"${trigg}\",
+                \"tags\": [
+                    {
+                        \"tag\": \"integ_itsm_grp\",
+                        \"value\": \"${SEGUNDO}\"
+                    },
+                    {
+                        \"tag\": \"integ_itsm_src\",
+                        \"value\": \"remota\"
+                    },
+                    {
+                        \"tag\": \"integ_itsm_type\",
+                        \"value\": \"cluster\"
+                    }
+                ]
+            },
+            \"auth\": \"${zabbix_auth_token}\",
+            \"id\": 1
+        }" ${zabbix_api}| awk -F\" '{print $10}');
 
-
-tagupdate=$(curl -s -H  'Content-Type: application/json-rpc' -d "
-{
-    \"jsonrpc\": \"2.0\",
-    \"method\": \"triggerprototype.update\",
-    \"params\": {
-        \"triggerid\": \"${triggerid}\",
-        \"tags\": [
-            {
-                \"tag\": \"dennis\",
-                \"value\": \"dennis\"
-            }
-        ]
-    },
-    \"auth\": \"${zabbix_auth_token}\",
-    \"id\": 1
-}" ${zabbix_api}| awk -F\" '{print $10}')
-
-echo $tagupdate
+        echo $tagupdate
+done
+done
