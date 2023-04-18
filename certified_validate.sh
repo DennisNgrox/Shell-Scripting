b1cd186a92b2b939df59777e8646fd288ac3635b
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2023-04-18 20:38:09 -0300

    Update certified_validate.sh

--- certified_validate.sh
+++ certified_validate.sh
@@ -8,7 +8,8 @@ then
             port=$2
 fi
 
-notAfter=$(openssl s_client -connect $host:$port < /dev/null 2> /dev/null | openssl x509 -in /dev/stdin -noout -dates | grep notAfter | sed -e 's/notAfter=//')
+notAfter=$(openssl s_client -connect $host:$port < /dev/null 2> /dev/null | openssl x509 -in /dev/stdin -noout -dates | grep notAfter | cut -d'=' -f2)
+expire=$(date -d "$notAfter" +%s)
 today=$(date --utc +%s)
 left_seconds=$((expire - today))
 left_days=$((left_seconds/60/60/24))
