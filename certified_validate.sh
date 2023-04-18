2327165536b3cfb73aeb110e5e3d8fc38a21510b
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2023-04-18 11:50:38 -0300

    Create certified_validate.sh
    Script para verificar tempo de validade do certificado.
    
    Passar como 1° parâmetro a url.

---
+++ certified_validate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+host="$1"
+if [ -z "$2" ]
+then
+          port=443
+  else
+            port=$2
+fi
+
+notAfter=$(openssl s_client -connect $host:$port < /dev/null 2> /dev/null | openssl x509 -in /dev/stdin -noout -dates | grep n>expire=$(date -d "$notAfter" +%s)
+today=$(date --utc +%s)
+left_seconds=$((expire - today))
+left_days=$((left_seconds/60/60/24))
+
+echo "$left_days"
