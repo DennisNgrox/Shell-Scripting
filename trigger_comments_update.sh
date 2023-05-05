e6344e8dc2c946c28fa8dbe8612706d385ed02bb
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2023-05-05 18:21:12 -0300

    Create trigger_comments_update.sh
    Execute o script passando como primeiro parâmetro o nome do host/template.
    Ex: ./trigger_update.sh "Zabbix Server"
    
    Precisa editar as seguintes variaveis:
    
    Nome do usuário:
    - zabbix_user 
    
    Senha do usuário:
    - zabbix_pass
    
    Url de acesso a API Zabbix:
    - zabbix_api
    
    Comentário a ser adicionado:
    - comments
    
    
    A execução do script realiza um get no "host_id" utilizando o nome do host e logo depois um get em todas as triggers pertencentes ao "host_id" afim de pegar o "trigger_id", todos os "trigger_id" são escritos em um arquivo por nome de "file.triggers" e então um loop é executado para que um comentário seja adicionado a cada "trigger_id" escrito no arquivo.
    
    Script Execusion -> host_id -> trigger_id -> loop -> logout

---
+++ trigger_comments_update.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+
+zabbix_user="Admin"
+zabbix_pass="Julia#2021"
+zabbix_api="http://192.168.32.128/zabbix/api_jsonrpc.php"
+primeiro=$1
+
+
+zabbix_auth_token=$(curl -s -k -H  'Content-Type: application/json-rpc' -d "{\"jsonrpc\": \"2.0\",\"method\":\"user.login\",\"params\":{\"user\":\""${zabbix_user}"\",\"password\":\""${zabbix_pass}"\"},\"auth\": null,\"id\":0}" $zabbix_api |  jq -r .result)
+
+hostid=$(curl -s -k -H  'Content-Type: application/json-rpc' -d "
+{
+    \"jsonrpc\": \"2.0\",
+    \"method\": \"template.get\", #Se for a nível de host, alterar para host.get
+    \"params\": {
+        \"output\": \"["hostid"]\",
+        \"filter\": {
+            \"host\": [
+                \"${primeiro}\"
+            ]
+        }
+    },
+    \"auth\": \"${zabbix_auth_token}\",
+    \"id\": 1
+}" ${zabbix_api}| awk -F\" '{print $10}')
+
+triggerid=$(curl -s -k -H  'Content-Type: application/json-rpc' -d "
+{
+    \"jsonrpc\": \"2.0\",
+    \"method\": \"trigger.get\",
+    \"params\": {
+        \"hostids\": \"${hostid}\",
+        \"output\": [\"triggerid\"]
+    },
+    \"auth\": \"${zabbix_auth_token}\",
+    \"id\": 1
+}" ${zabbix_api} | jq .result[].triggerid | sed s'/"//g' >> file.triggers)
+
+for i in $(cat file.triggers);
+do
+  value=$(echo $i);
+    comment=$(curl -s -k -H  'Content-Type: application/json-rpc' -d "
+   {
+        \"jsonrpc\": \"2.0\",
+        \"method\": \"trigger.update\",
+        \"params\": {
+            \"triggerid\": \"${value}\",
+            \"comments\": \"[Zabbix] Monitoracao de cluster, Monitor: Disponibilidade do Cluster, Intervalo: 5 minutos\" #Alterar para o que deseja
+        },
+        \"auth\": \"${zabbix_auth_token}\",
+        \"id\": 1
+    }" ${zabbix_api})
+
+
+
+echo $comment
+
+done
+
+logout=$(curl -s -k -H  'Content-Type: application/json-rpc' -d "
+{
+        \"jsonrpc\": \"2.0\",
+        \"method\": \"user.logout\",
+        \"params\": [],
+        \"id\": 1,
+        \"auth\": \"${zabbix_auth_token}\"
+}" ${zabbix_api})
